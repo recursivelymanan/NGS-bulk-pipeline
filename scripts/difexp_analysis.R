45a52b72cf166b56cb9797c87e9ef08e2c213e67
Manan <manan.zchopra@gmail.com>
2024-10-09 21:14:01 -0700

    Dev ongoing on R script

--- scripts/difexp_analysis.R
+++ scripts/difexp_analysis.R
@@ -3,33 +3,36 @@ library ("DESeq2")
 # Handle parameters from commandline
 args <- commandArgs(trailingOnly = TRUE)
 
-custom_comparisons == FALSE
+custom_comparisons <- FALSE
 if (length(args) >= 2) {
-    cts <- args[1]
-    col_data <- args[2]
-    comparisons <- unique(col_data$condition)
+    # Load in count and metadata tables
+    cts <- read.csv(args[1], sep = "\t", header = TRUE, row.names = 1)
+    col_data <- read.csv(args[2], header = TRUE)
 
     if (length(args) == 3) {
-        custom_comparisons == TRUE
+        custom_comparisons <- TRUE
         comparisons <- strsplit(args[3], split = ",")
+    }
+    else {
+        comparisons <- unique(col_data$condition)
     }
 } else {
     stop("Incorrect number of parameters passed.")
 }
 
 # Load in count and metadata tables
-cts <- read.table(cts, sep = "\t", header = TRUE)
-col_data <- read.table(col_data, sep = "\t", header = TRUE)
+#cts <- read.table(cts, sep = "\t", header = TRUE)
+#col_data <- read.table(col_data, sep = "\t", header = TRUE)
 
 # Run DESeq2
 dds <- DESeqDataSetFromMatrix(countData = cts,
                               colData = col_data,
                               design = ~ condition)
                         
-keep <- rowSums(counts(dds) >= 10) >= smallestGroupSize
+keep <- rowSums(counts(dds) >= 10) >= 1
 dds <- dds[keep, ]
 
-dds <- DeSeq(dds)
+dds <- DESeq(dds)
 
 # Get normalized counts and save file
 normalized_counts <- counts(dds, normalized = TRUE)
